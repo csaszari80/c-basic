ed928bc4d1f4c4502bd00535fa51ceb4bd0b1023
Császári Zoltán <csaszari80@gmail.com>
2017-11-23 15:01:44 +0100

    Változók és ciklusok

---
+++ Csharp alapok/04Ciklus/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Ciklus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i + 1);
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp alapok/Valtozok/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valtozok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Milyen legyen az ebéd");
+            Console.WriteLine("Vega");
+            Console.WriteLine("Egyébként pörkölt");
+            var valasz = Console.ReadLine(); 
+            if (valasz.ToLower() == "Vega".ToLower())
+            {
+                //ha péntek van
+                Console.WriteLine("Karfiol");
+                Console.WriteLine("Hagyma");
+                Console.WriteLine("Tejszín");
+            }
+            else
+            {
+                //ha name péntek van
+                Console.WriteLine("Marhalábszár");
+                Console.WriteLine("Hagyma");
+                Console.WriteLine("Pirospaprika");
+                Console.WriteLine("Szalonna");
+            }
+            Console.ReadLine();
+        }
+    }
+}
