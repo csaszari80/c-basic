c80ec0c065c6e7f27dddcd64ee03e0282fdfa2cd
Császári Zoltán <csaszari80@gmail.com>
2017-11-23 14:23:06 +0100

    Feltételek 1

---
+++ Csharp alapok/02Elagaztatas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Elagaztatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                //ha péntek van
+                Console.WriteLine("Karfiol");
+                Console.WriteLine("Hagyma");
+                Console.WriteLine("Tejszín");
+            }
+            else
+            {
+                //ha name péntek van
+                Console.WriteLine("Marhalábszár");
+                Console.WriteLine("Hagyma");
+                Console.WriteLine("Pirospaprika");
+                Console.WriteLine("Szalonna");
+            }
+            Console.ReadLine();
+        }
+    }
+}
