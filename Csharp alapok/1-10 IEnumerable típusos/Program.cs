b4ac291fe9e4977125c276f889fdfbc50c16b2b6
Császári Zoltán <csaszari80@gmail.com>
2017-12-28 16:07:14 +0100

    IEnumerable kész

---
+++ Csharp alapok/1-10 IEnumerable típusos/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_10_IEnumerable_típusos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // agyűjtemény létrehozásakor most megkerüljük az add függvényt a megfelelő konstruktorok generálásával és hívásával ez nem feltétlenül egyszerűbb
+            var adatok = new BejarhatoAdatok<Adatok>(
+                new Adatok[] {
+                    new Adatok(nev: "Első", szam: 1),
+                    new Adatok(nev: "Második", szam: 2),
+                    new Adatok(nev: "Harmadik", szam: 3),
+                    new Adatok(nev: "Negyedik", szam: 4)
+                }
+            );
+
+            // FONTOS: foreach cikluban a listát nem módosítjuk (lista típusnál hibát is dob rá)
+            foreach (var item in adatok)
+            {
+                Console.WriteLine("Név: {0}, Szám: {1}",item.Nev,item.Szam);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Egy IEnumerable felületet megvalósító osztály ezúttal szigorúan típusos megvalósítással
+    /// Ebben az implementálni kell típusos és típus nélküli GetEnumerator függvényeket is.
+    /// Továbbá a bejárást is osztályon belül oldjuk meg, Ehhez pedig nem csak az IEnumerable felületet, hanem az IEnumerator felületet is meg kell valósítani
+    /// </summary>
+    /// az osztályhivatkozásnak típusparamétert kell kapnia amit továbbad az IEnumerable és az IEnumerator felületnek
+    class BejarhatoAdatok<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        List<T> lista = new List<T>();
+        int pozicio = -1;
+        
+        /// <summary>
+        /// Ez a konstruktor amilyen típusó tömböt kap eredményű olyan típusú lesz a lista hoz létre
+        /// </summary>
+        /// <param name="adatok"></param>
+        public BejarhatoAdatok(T[] adatok)
+        {
+            lista = new List<T>(adatok);
+            
+        }
+
+        public T Current
+        {
+            get
+            {
+                return lista[pozicio];
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        /// <summary>
+        /// Ezt most nem valósítjuk meg mert macerás és nem ez a cél és tudom hogy nem szükséges
+        /// üresnek kell lennie mert a ciklusból való kilépéskor az objektum is meg kell szűnön
+        /// </summary>
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+      
+
+        public bool MoveNext()
+        {
+            pozicio++;
+            return pozicio < lista.Count();
+        }
+
+        public void Reset()
+        {
+            pozicio = -1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+    }
+
+    /// <summary>
+    /// Ezt az osztályt fogjuk gyűjteményben használni a lényeg, hogy nem csak egyféle tulajdonsága van így nem lehet ugyanúgy használni mint az előzőt(feldolgozáshoz castolni kell vagy a megfelelően kell megvalósítani a gyűjtemény bejárását
+    /// </summary>
+    class Adatok
+    {
+        public Adatok(string nev, int szam)
+        {
+            Nev = nev;
+            Szam = szam;
+        }
+
+        public string Nev { get; set; }
+
+        public int Szam { get; set; }
+    }
+    
+    
+
+}
--- Csharp alapok/1-10 IEnumerable/Program.cs
+++ Csharp alapok/1-10 IEnumerable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,12 @@ namespace _1_10_IEnumerable
     {
         static void Main(string[] args)
         {
-            foreach (var listaElem in BevasarloLista())
-            {
-                Console.WriteLine(listaElem);
-            }
-            //Valami ilyesmit szeretnénk elérni
+            //foreach (var listaElem in BevasarloLista())
+            //{
+            //    Console.WriteLine(listaElem);
+            //}
+
+            //Az előzővel Valami ilyesmit szeretnénk elérni
             //Console.WriteLine("1 kg kenyér");
             //Console.WriteLine("10 dkg felvágott");
             //Console.WriteLine("1 kg liszt");
@@ -24,10 +26,37 @@ namespace _1_10_IEnumerable
             //Console.WriteLine("1 kg Darálthús");
             //Console.WriteLine("1 kg tészta");
 
+
+            // feltöltjük a bejárható osztályt listaelemekkel
+            var lista = new BejarhatoOsztaly();
+            lista.Add("első");
+            lista.Add("második");
+            lista.Add("harmadik");
+            lista.Add("negyedik");
+            lista.Add("ötödik");
+            lista.Add("hatodik");
+
+            foreach (var elem in lista)
+            {
+                Console.WriteLine("-------foreach elem: {0}", elem); //bár objektumot kapunk vissza (nem típusos a megvalósítás) de mivel stringet tartalmaz ezért a cw tudja kezelni cast-olás nélkül is
+            }
+
+            //Console.WriteLine("--------------------------------------------------------------");
+
+            // A foreach lényegében ezt csinálja (ennél persze bonyolultabb de a lényege ez)
+            //var bejaro = lista.GetEnumerator();
+            //    while (bejaro.MoveNext())
+            //    {
+            //        var elem2 = bejaro.Current;
+            //        Console.WriteLine("-------while elem2: {0}", elem2);
+            //    }
+
             Console.ReadKey();
         }
-        //21 percnél tartok
-        private static IEnumerable<string> BevasarloLista() //speciális függvény az IEnumareble objektumlisták bejárását szolgálja ki jelen esetben stringeket ad vissza a foreach mindig a következő yield return értéket adja eredményül
+         
+
+        private static IEnumerable<string> BevasarloLista() //speciális függvény, az IEnumareble objektumlisták bejárását szolgálja ki jelen esetben stringeket ad vissza a foreach mindig a következő yield return értéket adja eredményül
+            /// az IEnumareable egy generikus osztály (paraméterei között típus is szerepel (<string>) ilyen még pl a List<típus>) ilyen módon használva szigorúan típusfüggő van nem típusfüggő változata is (nincs <típus>) ekkor object-eket ad vissza 
         {
             yield return "1 kg kenyér";
             yield return "10 dkg felvágott";
@@ -37,6 +66,85 @@ namespace _1_10_IEnumerable
             yield return "Nasi";
             yield return "1 kg Darálthús";
             yield return "1 kg tészta";
+        }
+
+        /// <summary>
+        /// Egy IEnumerable felületet megvalósító osztály (típus nélkül)
+        /// Ebben az esetben objektumokat kapunk vissza a régebbi ADONET osztálykönvtárakban találkozahtunk vele hátrányam hogy a feldolgozáshoz megfelelően cast-olni kell ehhez pedik pontosan kell tudni mivel tér vissza
+        /// </summary>
+        class BejarhatoOsztaly : IEnumerable
+        {
+            List<string> lista = new List<string>(); // ez a stringekból álló lista nevű változó fogja tartalmazni az adatokat egyúttal példányosítva is van
+
+            //Mivel nem akarom publikálni a listát ezért kell egy publikus függvény amivel elemeket lehet hozzáadni
+            public void Add(string elem)
+            {
+                lista.Add(elem);
+            }
+            
+            /// <summary>
+            /// Az enumerátornak be kell tudni járni az osztály gyűjteményét
+            /// Ezt megoldhatjuk külön osztályban this használatával
+            /// Most a Bejaro osztály segítségével fogjuk elvégezni a bejárást.
+            /// </summary>
+            /// <returns></returns>
+            public IEnumerator GetEnumerator()
+            {
+                Console.WriteLine("     Getenumerator hívása");
+                return new Bejaro(lista);
+            }
+        }
+        class Bejaro : IEnumerator  //A bejáráshoz az IEnumeratort kell implementálni (Current MoveNext és Reset függvények)
+        {
+            // ennek fogja átadni a BejarhatoOsztaly IEnumerator függvényének a paraméterét
+            private List<string> lista;
+
+            int pozicio = -1; // a kezdő pozíció
+
+            /// <summary>
+            /// A konstruktor értéket ad a lista változónak
+            /// </summary>
+            /// <param name="lista"></param>
+            public Bejaro(List<string> lista)
+            {
+                this.lista = lista;
+            }
+
+            /// <summary>
+            /// Vissza adja a lista aktuális pozíciójában lévő elemet
+            /// </summary>
+            public object Current
+            {
+                get
+                {
+                    var current = lista[pozicio];
+
+                    Console.WriteLine("     Current ( pozíció: {0}, elem: {1} )",pozicio,current);
+
+                    return current;
+                }
+            }
+
+            /// <summary>
+            /// Növeli a pozíciót és megmondja, hogy van-e még elem
+            /// </summary>
+            /// <returns></returns>
+            public bool MoveNext()
+            {
+                pozicio++;
+                var vanMegElem = pozicio < lista.Count;
+                Console.WriteLine("     MoveNext ( pozíció: {0}, VanMeg: {1} )", pozicio, vanMegElem);
+
+                return vanMegElem;
+            }
+
+            /// <summary>
+            /// Visszaállítja a pozíció kezdőértékét
+            /// </summary>
+            public void Reset()
+            {
+                pozicio = -1;
+            }
         }
     }
 }
